98a31d767bd34a736ed0568c9693c100010ad7f7
Ahmed-Moudud <suzan.4bd@gmail.com>
2022-11-25 19:24:14 +0600

    api for all products implemented

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const cors = require('cors');
-const port = process.env.PORT || 5000;
 const { MongoClient, ServerApiVersion } = require('mongodb');
+require('dotenv').config()
+const port = process.env.PORT || 5000;
 
 const app = express();
 
@@ -12,14 +13,39 @@ app.use(express.json());
 
 
 
-const uri = "mongodb+srv://<username>:<password>@cluster0.y6l8m1u.mongodb.net/?retryWrites=true&w=majority";
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.y6l8m1u.mongodb.net/?retryWrites=true&w=majority`;
+console.log(uri);
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-client.connect(err => {
-  const collection = client.db("test").collection("devices");
-  // perform actions on the collection object
-  client.close();
-});
 
+async function run(){
+    try {
+        const productCollection = client.db('bikeMart').collection('products');
+
+        app.get('/products', async(req, res) => {
+            const query = {};
+            const options = await productCollection.find(query).limit(3).toArray();
+            res.send(options);
+        })
+
+        app.get('/allproducts', async(req, res) => {
+            const query = {};
+            const options = await productCollection.find(query).toArray();
+            res.send(options);
+        })
+
+        app.get('/products/:category', async(req, res) => {
+            const category = req.params.category;
+            const query = {category};
+            const items = await productCollection.find(query).toArray();
+            res.send(items);
+        })
+    }
+    finally{
+
+    }
+}
+
+run().catch(console.log());
 
 app.get('/', async(req, res) => {
     res.send('bike mart server is running')
